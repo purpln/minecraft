2f49bbba090c512ef0b667d24d42b86dd5daa84a
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-01-24 17:14:22 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -5,5 +5,13 @@ import PackageDescription
 let package = Package(
     name: "minecraft",
     products: [.library(name: "Minecraft", targets: ["Minecraft"])],
-    targets: [.target(name: "Minecraft", resources: [.process("docs/blocks.json")])]
+    targets: [.target(name: "Minecraft", resources: [
+        .process("docs/biomes.json"),
+        .process("docs/blocks.json"),
+        .process("docs/collisions.json"),
+        .process("docs/items.json"),
+        .process("docs/protocol.json"),
+        .process("docs/recipes.json"),
+        .process("docs/states.json")
+    ])]
 )
--- Sources/Minecraft/Minecraft.swift
+++ Sources/Minecraft/Minecraft.swift
@@ -6,6 +6,12 @@ open class Minecraft {
 
 public extension Minecraft {
     static var blocks: [[String:Any]]? { data.blocks }
+    static var items: [[String:Any]]? { data.items }
+    static var recipes: [String:Any]? { data.recipes }
+    static var biomes: [[String:Any]]? { data.biomes }
+    static var collisions: [String:Any]? { data.collisions }
+    static var states: [[String:Any]]? { data.states }
+    static var `protocol`: [String:Any]? { data.protocol }
 }
 
 extension Minecraft {
@@ -13,9 +19,21 @@ extension Minecraft {
         static var shared = Objects()
         
         var blocks: [[String:Any]]?
+        var items: [[String:Any]]?
+        var recipes: [String:Any]?
+        var biomes: [[String:Any]]?
+        var collisions: [String:Any]?
+        var states: [[String:Any]]?
+        var `protocol`: [String:Any]?
         
         init() {
             blocks = "blocks.json".data?.dict as? [[String:Any]]
+            items = "items.json".data?.dict as? [[String:Any]]
+            recipes = "recipes.json".data?.dict as? [String:Any]
+            biomes = "biomes.json".data?.dict as? [[String:Any]]
+            collisions = "collisions.json".data?.dict as? [String:Any]
+            states = "states.json".data?.dict as? [[String:Any]]
+            `protocol` = "protocol.json".data?.dict as? [String:Any]
         }
     }
 }
