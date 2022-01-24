5238d1cde15b41062e2bf209c58d6997840fde52
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2022-01-24 16:34:58 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -5,6 +5,5 @@ import PackageDescription
 let package = Package(
     name: "minecraft",
     products: [.library(name: "package", targets: ["Minecraft"])],
-    dependencies: [],
     targets: [.target(name: "Minecraft", resources: [.process("docs/blocks.json")])]
 )
